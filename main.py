c700742d28ceee9ae20326072a34061784bf58ec
Aditya Mansana <33343968+adityamansana@users.noreply.github.com>
2026-04-01 01:08:32 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
-
+from typing import List
+from pydantic import BaseModel, Field
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+from zod import *
+from langchain import createAgent, providerStrategy
+
+tavily = TavilyClient()
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+class Source(BaseModel):
+    """Schema for a source to be used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    url: str = Field(description="The agent's answer to the query")
+    sources:List = Field(default_factory=list,description="List of sources used to generate the answer.")
+
+llm = ChatOpenAI(model="gpt-4.1-mini")
+tools = [search]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+# agent = create_agent(model=llm,tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    information = """
-    Elon Reeve Musk FRS (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman, known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
-
-Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He received bachelor's degrees from the University of Pennsylvania in 1997 before moving to California, United States, to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
-
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
-
-Musk was the largest donor in the 2024 U.S. presidential election, and is a supporter of global far-right figures, causes, and political parties. In early 2025, he served as senior advisor to United States president Donald Trump and as the de facto head of DOGE. After a public feud with Trump, Musk left the Trump administration and announced he was creating his own political party, the America Party.
-
-Musk's political activities, views, and statements have made him a polarizing figure, especially following the COVID-19 pandemic. He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation and promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. His role in the second Trump administration attracted public backlash, particularly in response to DOGE.
-    """
-
-    summary_template = """
-    given the information {information} about a person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
-
-    llm1 = ChatOllama(temperature=0, model="gemma3:270m",)
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
-    chain = summary_prompt_template | llm
-    chain1 = summary_prompt_template | llm1
-    print("\n\n\n\nOpen AI")
-
-    response = chain.invoke(input={"information": information})
-    print(response.content)
-    print("\n\n\n\nChat Ollama")
-    response = chain1.invoke(input={"information": information})
-    formatted = response.content.replace(". ", ".\n")
-
-    print(formatted)
-
+    content = "search for 3 job postings for an Senior Sdet Playwright with 11 years experience using langchain in India with Remote work mode on linkedin and list their details"
+    # content = "what is the weather prediction for pune for the month of April 2026"
+    result = agent.invoke({"messages":HumanMessage(content=content)})
+    print(result)
+    print(result['structured_response'].answer)
 
 if __name__ == "__main__":
     main()
